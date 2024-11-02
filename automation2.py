b44c6dd793fddfe9a18bec44220a9bd8bbc126d0
alpha <alpha5611331@gmail.com>
2024-11-02 09:25:21 -0400

    added 'Collection' column, removed body text

--- automation2.py
+++ automation2.py
@@ -1,11 +1,11 @@
 import csv
 import os
+import random
 import time
-import tkinter as tk
 import urllib
 import urllib.parse
+from datetime import datetime, timedelta
 from pathlib import Path
-from tkinter import messagebox
 from typing import Dict, List
 
 import requests
@@ -14,7 +14,6 @@ from loguru import logger
 CUR_DIR = Path(__file__).parent
 IMAGE_DIR = CUR_DIR / "images"
 OUTPUT_CSV = CUR_DIR / "output.csv"
-BODY_FILE_PATH = CUR_DIR / "body.txt"
 HTTP_TIMEOUT = 15.0
 IMAGE_POSITIONS = {
     "1": "CamGallery",
@@ -36,18 +35,19 @@ def extract_product_info(image_filename: str) -> Dict[str, str]:
     }
 
 
-def msgbox_info(info_txt: str) -> None:
-    root = tk.Tk()
-    root.withdraw()
-    messagebox.showwarning("Info", info_txt)
-    root.quit()
+def get_random_time_in_date_range(start_year, start_month, end_year, end_month) -> str:
+    """Returns random date in range. For example `October 2023`."""
+    start_date = datetime(start_year, start_month, 1)
+    if end_month == 12:
+        end_date = datetime(end_year + 1, 1, 1)
+    else:
+        end_date = datetime(end_year, end_month + 1, 1)
 
+    delta = end_date - start_date
+    random_seconds = random.randint(0, int(delta.total_seconds()))
 
-def msgbox_warning(url: str) -> None:
-    root = tk.Tk()
-    root.withdraw()
-    messagebox.showwarning("Image Check", f"{url}")
-    root.quit()
+    random_time = start_date + timedelta(seconds=random_seconds)
+    return random_time.strftime("%B %Y")
 
 
 def check_image_exists(image_link: str) -> bool:
@@ -66,111 +66,104 @@ def check_image_exists(image_link: str) -> bool:
 
 
 def create_inventory_csv(image_dir: str, output_csv: str) -> None:
-    with BODY_FILE_PATH.open("r") as body_file:
-        body_text = body_file.read()
+    image_info_list: List[Dict[str, str]] = []
+    for image_filename in os.listdir(image_dir):
+        file_ext = os.path.splitext(image_filename)[-1]
+        if file_ext not in [".jpg", ".jpeg", ".png"]:
+            continue
 
-        image_info_list: List[Dict[str, str]] = []
-        for image_filename in os.listdir(image_dir):
-            file_ext = os.path.splitext(image_filename)[-1]
-            if file_ext not in [".jpg", ".jpeg", ".png"]:
-                continue
+        image_info = extract_product_info(image_filename=image_filename)
+        for pos_index, pos_name in IMAGE_POSITIONS.items():
+            image_name = f'{image_info["title"]}_{image_info["type"]}_{pos_name}.png'
+            logger.info(f"image_name: {image_name}")
 
-            image_info = extract_product_info(image_filename=image_filename)
-            for pos_index, pos_name in IMAGE_POSITIONS.items():
-                image_name = (
-                    f'{image_info["title"]}_{image_info["type"]}_{pos_name}.png'
-                )
-                logger.info(f"image_name: {image_name}")
+            image_src_link = (
+                f"{IMAGE_HOST_URL}{urllib.parse.quote(image_name)}?v={time.time()}"
+            )
 
-                image_src_link = (
-                    f"{IMAGE_HOST_URL}{urllib.parse.quote(image_name)}?v={time.time()}"
-                )
-
-                if CHECK_IMAGE_LINK:
-                    if check_image_exists(image_src_link):
-                        logger.info("exists")
-                    else:
-                        logger.warning(f"not found: {image_src_link}")
-                        msgbox_warning(f"Image not found: {image_src_link}")
-
-                if pos_index == "1":
-                    image_info_list.append(
-                        {
-                            "Handle": image_info["handle"],
-                            "Title": image_info["title"],
-                            "Body (HTML)": body_text,
-                            "Vendor": "My Store",
-                            "Product Category": "Software > Digital Goods & Currency > Digital Artwork",
-                            "Type": image_info["type"],
-                            "Tags": "",
-                            "Published": "TRUE",
-                            "Option1 Name": "Title",
-                            "Option1 Value": "Default Title",
-                            "Option1 Linked To": "",
-                            "Option2 Name": "",
-                            "Option2 Value": "",
-                            "Option2 Linked To": "",
-                            "Option3 Name": "",
-                            "Option3 Value": "",
-                            "Option3 Linked To": "",
-                            "Variant SKU": "",
-                            "Variant Grams": "0",
-                            "Variant Inventory Tracker": "shopify",
-                            "Variant Inventory Qty": "0",
-                            "Variant Inventory Policy": "continue",
-                            "Variant Fulfillment Service": "manual",
-                            "Variant Price": "485",
-                            "Variant Compare At Price": "",
-                            "Variant Requires Shipping": "TRUE",
-                            "Variant Taxable": "TRUE",
-                            "Variant Barcode": "",
-                            "Image Src": image_src_link,
-                            "Image Position": pos_index,
-                            "Image Alt Text": "",
-                            "Gift Card": "FALSE",
-                            "SEO Title": "",
-                            "SEO Description": "",
-                            "Google Shopping / Google Product Category": "",
-                            "Google Shopping / Gender": "",
-                            "Google Shopping / Age Group": "",
-                            "Google Shopping / MPN": "",
-                            "Google Shopping / Condition": "",
-                            "Google Shopping / Custom Product": "",
-                            "Google Shopping / Custom Label 0": "",
-                            "Google Shopping / Custom Label 1": "",
-                            "Google Shopping / Custom Label 2": "",
-                            "Google Shopping / Custom Label 3": "",
-                            "Google Shopping / Custom Label 4": "",
-                            "Variant Image": "",
-                            "Variant Weight Unit": "lb",
-                            "Variant Tax Code": "",
-                            "Cost per item": "",
-                            "Included / United States": "TRUE",
-                            "Price / United States": "",
-                            "Compare At Price / United States": "",
-                            "Included / International": "TRUE",
-                            "Price / International": "",
-                            "Compare At Price / International": "",
-                            "Status": "active",
-                        }
-                    )
+            if CHECK_IMAGE_LINK:
+                if check_image_exists(image_src_link):
+                    logger.info("exists")
                 else:
-                    image_info_list.append(
-                        {
-                            "Handle": image_info["handle"],
-                            "Image Src": image_src_link,
-                            "Image Position": pos_index,
-                        }
-                    )
+                    logger.warning(f"image not found: {image_src_link}")
+
+            if pos_index == "1":
+                image_info_list.append(
+                    {
+                        "Handle": image_info["handle"],
+                        "Title": image_info["title"],
+                        "Body (HTML)": "",
+                        "Vendor": "My Store",
+                        "Product Category": "Software > Digital Goods & Currency > Digital Artwork",
+                        "Type": image_info["type"],
+                        "Tags": "",
+                        "Published": "TRUE",
+                        "Collection": get_random_time_in_date_range(2023, 1, 2024, 11),
+                        "Option1 Name": "Title",
+                        "Option1 Value": "Default Title",
+                        "Option1 Linked To": "",
+                        "Option2 Name": "",
+                        "Option2 Value": "",
+                        "Option2 Linked To": "",
+                        "Option3 Name": "",
+                        "Option3 Value": "",
+                        "Option3 Linked To": "",
+                        "Variant SKU": "",
+                        "Variant Grams": "0",
+                        "Variant Inventory Tracker": "shopify",
+                        "Variant Inventory Qty": "0",
+                        "Variant Inventory Policy": "continue",
+                        "Variant Fulfillment Service": "manual",
+                        "Variant Price": "485",
+                        "Variant Compare At Price": "",
+                        "Variant Requires Shipping": "TRUE",
+                        "Variant Taxable": "TRUE",
+                        "Variant Barcode": "",
+                        "Image Src": image_src_link,
+                        "Image Position": pos_index,
+                        "Image Alt Text": "",
+                        "Gift Card": "FALSE",
+                        "SEO Title": "",
+                        "SEO Description": "",
+                        "Google Shopping / Google Product Category": "",
+                        "Google Shopping / Gender": "",
+                        "Google Shopping / Age Group": "",
+                        "Google Shopping / MPN": "",
+                        "Google Shopping / Condition": "",
+                        "Google Shopping / Custom Product": "",
+                        "Google Shopping / Custom Label 0": "",
+                        "Google Shopping / Custom Label 1": "",
+                        "Google Shopping / Custom Label 2": "",
+                        "Google Shopping / Custom Label 3": "",
+                        "Google Shopping / Custom Label 4": "",
+                        "Variant Image": "",
+                        "Variant Weight Unit": "lb",
+                        "Variant Tax Code": "",
+                        "Cost per item": "",
+                        "Included / United States": "TRUE",
+                        "Price / United States": "",
+                        "Compare At Price / United States": "",
+                        "Included / International": "TRUE",
+                        "Price / International": "",
+                        "Compare At Price / International": "",
+                        "Status": "active",
+                    }
+                )
+            else:
+                image_info_list.append(
+                    {
+                        "Handle": image_info["handle"],
+                        "Image Src": image_src_link,
+                        "Image Position": pos_index,
+                    }
+                )
 
     with open(output_csv, "w", newline="", encoding="utf-8-sig") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=image_info_list[0].keys())
         writer.writeheader()  # Write the header
         writer.writerows(image_info_list)  # Write the data
 
-    msg = f"Data saved to {output_csv}"
-    logger.info(msg)
-    msgbox_info(msg)
+    logger.info(f"Data saved to {output_csv}")
 
 
 def main():
